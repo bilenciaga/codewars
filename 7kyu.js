82b1ec800b378fb97c751d76ff18e462cb5c7270
Trung Nguyen <nghtrung1001@gmail.com>
2022-11-06 14:10:43 +0900

    Update 7kyu.js

--- 7kyu.js
+++ 7kyu.js
@@ -1,3 +1,11 @@
 // friend or foe 
 // const solution = array => array.filter(friend => friend.length == 4)
 // console.log(solution(['nhan', 'nam', 'tung', 'hien', 'lam']))
+
+// const oddOrEven = array => {
+//   const sum = array.reduce((p,c) => p + c, 0);
+//   console.log(sum)
+//   if (sum % 2)  { return 'odd' }
+//   else {return 'even'}
+// }
+// console.log(oddOrEven([3, 2, 3, 4]))
